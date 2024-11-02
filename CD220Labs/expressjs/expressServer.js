3a653ab0909cb9d167df21853ba35ab849df9edb
sherelynn <sherelynnb@missionreadyhq.com>
2024-11-03 10:19:32 +1300

    Completed Task 1: Added endpoint /fetchMonth/:num

--- CD220Labs/expressjs/expressServer.js
+++ CD220Labs/expressjs/expressServer.js
@@ -1,34 +1,68 @@
- // Import the Express.js library
-const express = require('express');
+// Import the Express.js library
+const express = require("express")
 
 // Create an instance of an Express application
-const app = new express();
+const app = new express()
 
 // Initialize an array to store login details
-let loginDetails = [];
+let loginDetails = []
 
 // Define the root route to send a welcome message
 app.get("/", (req, res) => {
-    res.send("Welcome to the express server");
-});
+  res.send("Welcome to the express server")
+})
 
 // Define a route to send login details as a JSON string
 app.get("/loginDetails", (req, res) => {
-    res.send(JSON.stringify(loginDetails));
-});
+  res.send(JSON.stringify(loginDetails))
+})
 
 // Define a route to handle login requests and store login details
 app.post("/login/:name", (req, res) => {
-    loginDetails.push({ "name": req.params.name, "login_time": new Date() });
-    res.send(req.params.name + ", You are logged in!");
-});
+  loginDetails.push({ name: req.params.name, login_time: new Date() })
+  res.send(req.params.name + ", You are logged in!")
+})
 
 // Define a dynamic route to greet users by name
 app.get("/:name", (req, res) => {
-    res.send("Hello " + req.params.name);
-});
+  res.send("Hello " + req.params.name)
+})
+
+// ========== Exercise 1: Add your own endpoint  ========== //
+// Create a list with the names of the month
+// Add an endpoint in the code /fetchMonth/:num which will fetch a particular month from a list and return it to user
+// test: curl http://localhost:3333/fetchMonth/1
+
+// Define an array containing the names of the months
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+]
+// Define a route to fetch the month name based on a given number
+app.get("/fetchMonth/:num", (req, res) => {
+  // Parse the number from the request parameters
+  let num = parseInt(req.params.num)
+  // Check if the number is a valid month number
+  if (num < 1 || num > months.length) {
+    // Send an error message if the number is not valid
+    res.send("Not a valid month number")
+  } else {
+    // Send the corresponding month name if the number is valid
+    res.send(months[num - 1])
+  }
+})
 
 // Start the server and listen on port 3333
 app.listen(3333, () => {
-    console.log(`Listening at http://localhost:3333`);
-});
+  console.log(`Listening at http://localhost:3333`)
+})
